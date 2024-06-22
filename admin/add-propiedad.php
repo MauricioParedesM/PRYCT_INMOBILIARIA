646566000e2d8120b1ca9899e1414159b0a4372e
MauParedesM <paredesmauricio777@gmail.com>
2024-06-21 23:57:49 -0400

    first

--- admin/add-propiedad.php
+++ admin/add-propiedad.php
@@ -160,20 +160,20 @@ if (isset($_POST['agregar'])) {
 
                     <div class="fila">
                     <div class="box">
-    <label for="dormitorios">Dormitorios</label>
-    <select name="dormitorios" class="input-entrada-texto">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-    </select>
-</div>
+                        <label for="dormitorios">Dormitorios</label>
+                        <select name="dormitorios" class="input-entrada-texto">
+                        <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+                    </div>
 
 
                         <div class="box">
